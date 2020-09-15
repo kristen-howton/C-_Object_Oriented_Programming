52c08fb45419d26512a727191dbb908a82e3b85d
Kristen Howton <kristen.howton@gmail.com>
2020-09-15 16:35:36 -0500

    customer model created

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        public string HomeAddress { get; set; }
+        public string WorkAddress { get; set; }
+    }
+}
