c4dc1216e415b18853f39091c34985d9058ffeb5
Kristen Howton <kristen.howton@gmail.com>
2020-09-17 16:06:08 -0500

    simple customer full name test

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -4,10 +4,18 @@ namespace ACM.BL
 {
     public class Customer
     {
+
         public string LastName { get; set; }
         public string FirstName { get; set; }
-        public string Email { get; set; }
-        public string HomeAddress { get; set; }
-        public string WorkAddress { get; set; }
+        public  string  EmailAddress { get; set; }
+        public int CustomerId { get; private set; }
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+        }
     }
 }
+    
---
+++ Tests/ACM.BLTest/CustomerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTests
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //Arrange
+            //Instance of customer class
+            Customer customer = new Customer
+            {
+                //using object initatior
+                FirstName = "Ann",
+                LastName = "Perkins"
+            };
+            string expected = "Perkins, Ann";
+
+            //Act
+            string actual = customer.FullName;
+            //Assert 
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
